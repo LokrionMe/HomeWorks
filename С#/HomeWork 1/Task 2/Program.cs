33d40a4c12a81e4032dadc5dbc93ea41f0debb5a
Lokrion <m.kostian@yandex.ru>
2022-08-20 01:12:18 +0700

    task 2

---
+++ С#/HomeWork 1/Task 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input first integer number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"max = {num1}, min = {num2}");
+}
+else
+{
+    Console.WriteLine($"max = {num2}, min = {num1}");
+}

2bc933dc41daded44fc95fe33237aafb45826e56
Lokrion <m.kostian@yandex.ru>
2022-08-20 01:34:40 +0700

    task 4

---
+++ С#/HomeWork 1/Task 4/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Input first integer number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third integer number:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max)
+{
+    max = num2;
+    if (num3 > max)
+    {
+        max = num3;
+        Console.WriteLine($"max = {num3}");
+    }
+    else
+    {
+        Console.WriteLine($"max = {num2}");
+    }
+}
+else
+{
+    if (num3 > max)
+    {
+        max = num3;
+        Console.WriteLine($"max = {num3}");
+    }
+    else
+    {
+        Console.WriteLine($"max = {num1}");
+    }
+}

f85c6c94aa0d62b5c9883a1eeb765ce1975ffcb8
Lokrion <m.kostian@yandex.ru>
2022-08-24 22:38:38 +0700

    Task 13

---
+++ С#/HomeWork 2/Task 13/Program.cs
@@ -0,0 +1,8 @@
+﻿void ShowThirdNumber(int num)
+{
+    if (num>99) Console.WriteLine(num%1000/100);
+    else Console.WriteLine("Third number is empty");
+}
+Console.Write("Input positive integer number:");
+int num = Convert.ToInt32(Console.ReadLine());
+ShowThirdNumber(num);

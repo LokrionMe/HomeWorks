4c868fd44869b8b16711e07b61904c5291e27ebe
Lokrion <m.kostian@yandex.ru>
2022-08-24 22:49:44 +0700

    Task 15

---
+++ С#/HomeWork 2/Task 15/Program.cs
@@ -0,0 +1,10 @@
+﻿bool CheckDay(int num)
+{
+    if ((num>5)&(num<8)) return true;
+    return false;
+}
+Console.Write("Input positive integer number:");
+int num = Convert.ToInt32(Console.ReadLine());
+bool result = CheckDay(num);
+if (result == true) Console.WriteLine("Weekend");
+else Console.WriteLine("No Weekend");

741e975f8283b7f78ec1fbee5aa61a8b697d7241
Lokrion <m.kostian@yandex.ru>
2022-08-26 21:34:26 +0700

    task 19

---
+++ С#/HomeWork 3/Task 19/Program.cs
@@ -0,0 +1,23 @@
+﻿bool CheckPolyndrom(int num)
+{
+    int dev = 0;
+    int num3 = 0;
+    int num2 = num;
+    if (num / 10 == 0) return false;
+    else
+    {
+        while (num2 != 0)
+        {
+        dev = num2 % 10;
+        num2 = num2 / 10;
+        num3 = num3 *10 + dev;
+        }
+    }
+    if (num == num3) return true;
+    else 
+    return false;
+}
+Console.Write("Input first integer number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (CheckPolyndrom(num1) == true) Console.WriteLine($"{num1} - polyndrom");
+else Console.WriteLine($"{num1} - NOT polyndrom");

531eecd164c53c6119016d817efde6610f234d9b
Lokrion <m.kostian@yandex.ru>
2022-08-26 21:38:42 +0700

    task 23

---
+++ С#/HomeWork 3/Task 23/Program.cs
@@ -0,0 +1,12 @@
+﻿void Sqr(int num)
+{
+    int i = 1;
+    while (i<=num) 
+    {
+        Console.Write($"{i*i*i} ");
+        i ++;
+    }
+}
+Console.Write("Input integer number:");
+int number = Convert.ToInt32(Console.ReadLine());
+Sqr(number);

57edeeef2d9418974e4fe660b8a41fcd737e8cb1
Lokrion <m.kostian@yandex.ru>
2022-08-31 19:46:26 +0700

    Task 27

---
+++ С#/HomeWork 4/Task 27/Program.cs
@@ -0,0 +1,14 @@
+﻿int SumOfNumbers(int num)
+{
+    int sum = 0;
+    while (num != 0)
+        {
+        sum = sum + num % 10;
+        num /= 10;
+        }
+    return sum;
+}
+Console.Write("Input integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = SumOfNumbers(num);
+Console.WriteLine($"Amount numbers of {num} = {sum}");

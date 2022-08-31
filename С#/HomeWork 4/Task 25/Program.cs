7d056c3d47c0493b8db0ec921d3151617690f138
Lokrion <m.kostian@yandex.ru>
2022-08-31 19:34:39 +0700

    Task 25

---
+++ С#/HomeWork 4/Task 25/Program.cs
@@ -0,0 +1,15 @@
+﻿int sqr (int num, int step)
+{
+    int pr = 1;
+    for (int i = 1; i <= step; i++)
+    {
+        pr *= num;
+    }
+    return pr;
+}
+Console.Write("Input integer number");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input degree");
+int level = Convert.ToInt32(Console.ReadLine());
+int number = sqr(num, level);
+Console.WriteLine($"{num} in {level} = {number}");

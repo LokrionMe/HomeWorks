45ee9363765baba46487957fcfc439d7f1ec65c9
Lokrion <m.kostian@yandex.ru>
2022-09-07 21:34:41 +0700

    task 43

--- С#/HomeWork 6/Task 43/Program.cs
+++ С#/HomeWork 6/Task 43/Program.cs
@@ -1,30 +1,9 @@
-﻿/*хотел посмотреть насколько большую и глубокую выборку можно сделать: при х от -100 до 100
-и при шаге в 0,1 консоль просто завершает свою работу не отобразив ничего.
-хотя всё что касается шага 0,5 всё работает стабильно и корректно
-и да я знаю что больше 3 переменных "некрасиво" добавлять в метод но кто мне запретит))*/ 
-
-void IntersectionOfLines (double b1, double k1, double b2, double k2, double min, double max, double step)
+﻿void IntersectionOfLines (double b1, double k1, double b2, double k2)
 {
-    double x = min;
-    while (x < max)
-    {
-        double y1 = k1 * x + b1;
-        double y2 = k2 * x + b2;
-        if (y1 == y2) 
-        {
-            Console.WriteLine($"Itersection point of lines ({x};{y1}) on range ({min};{max})");
-            break;
-        } 
-        x += step ;
-    }        
-    if (x == max) Console.WriteLine($"Intersection point is empty on range ({min};{max})");   
+    double x = (b2 - b1)/(k1-k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Intersection point ({x:f2};{y:f2})");
 }
-Console.Write("Input minimum of range: ");
-double min = Convert.ToDouble(Console.ReadLine());
-Console.Write("Input maximum of range(except last number): ");
-double max = Convert.ToDouble(Console.ReadLine());
-Console.Write("Input step: ");
-double step = Convert.ToDouble(Console.ReadLine());
 Console.Write("Input b1: ");
 double b1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Input k1: ");
@@ -33,4 +12,4 @@ Console.Write("Input b2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Input k2: ");
 double k2 = Convert.ToDouble(Console.ReadLine());
-IntersectionOfLines(b1, k1, b2, k2, min, max, step);
+IntersectionOfLines(b1, k1, b2, k2);

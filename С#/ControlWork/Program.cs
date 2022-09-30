8d8e4133ec2ddc077b07df28783110fd927a1215
Lokrion <m.kostian@yandex.ru>
2022-09-30 20:23:47 +0700

    task complete

--- С#/ControlWork/Program.cs
+++ С#/ControlWork/Program.cs
@@ -15,10 +15,35 @@ void PrintArray (string[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length;i++)
     {
-        if (i<array.Length - 1) Console.WriteLine($"{array[i]}");
+        if (i<array.Length - 1) Console.WriteLine($"{array[i]};");
         if (i==array.Length - 1) Console.Write($"{array[i]}]");
     }
     Console.WriteLine();
 }
+int checkSize(string [] array)
+{
+    int k = 0;
+    for(int i = 0;i < array.Length; i++)
+    {
+        if (array[i].Length<4) k++;
+    }
+    return k;
+}
+string [] ExcludeBigPhrase(string [] array)
+{
+    string [] newArray = new string [checkSize(array)];
+    int k = 0;
+    for (int i = 0; i < array.Length;i++)
+    {
+        if (array[i].Length<4)
+        {
+            newArray[k] = array[i];
+            k++;
+        }
+    }
+    return newArray;
+}
 string[] NewArray = CreateArray();
 PrintArray(NewArray);
+string[] chArray = ExcludeBigPhrase(NewArray);
+PrintArray(chArray);

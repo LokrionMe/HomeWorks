16ade08fdb2a7275b1054a494ce65f9ba5377811
Lokrion <m.kostian@yandex.ru>
2022-09-30 20:07:45 +0700

    Create array

---
+++ С#/ControlWork/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] CreateArray()
+{
+    Console.Write("Input ammount of phrases: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Input phrase: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
